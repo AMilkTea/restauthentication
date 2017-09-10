3219ab09b1219508269dc6129066a998cf6ab553
YonghuiLv <821704108@qq.com>
2017-09-10 19:00:37 +0500

    init project

---
+++ src/test/java/com/muqi/restauthentication/RestauthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muqi.restauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestauthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
